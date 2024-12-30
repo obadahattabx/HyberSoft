fc4d3be6f6aaab7ca2e63b1c8d894cdaf9d17bb5
yazandoha <yazandoha@hotmail.com>
2024-12-30 10:28:20 +0300

    add home and services

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 
+import Home from './component/Home/Home';
 import Navbar from './component/Navbar/Navbar';
+import Services from './component/Services/Services';
 export default function App() {
   return (
+    <>
     <Navbar/>
+    <Home/>
+    <Services/>
+    </>
   )
 }
