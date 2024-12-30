b64b8d633722cad8cbabc405c55ced259e7c7781
yazandoha <yazandoha@hotmail.com>
2024-12-30 13:08:36 +0300

    add feacher and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 
+import Footer from './component/Footer/Footer';
 import Home from './component/Home/Home';
 import Navbar from './component/Navbar/Navbar';
+import Service from './component/Service/Service';
 import Services from './component/Services/Services';
 export default function App() {
   return (
     <>
     <Navbar/>
     <Home/>
+    <Service/>
     <Services/>
+    <Footer/>
     </>
   )
 }
---
+++ src/component/Service/Service.jsx
@@ -0,0 +1,108 @@
+import './Service.css';
+export default function Service() {
+  return (
+    <>
+      <section className="service-area pt-140 pb-110">
+        <div className="container">
+          <div className="row">
+            <div className="mx-auto col-xl-6 col-lg-7 col-md-10">
+              <div className="text-center section-title mb-60">
+                <h1>Why Choose Us</h1>
+                <p>
+                  Lorem ipsum dolor sit amet, consetetur sadipscing elitr sed
+                  diam nonumy eirmod tempor invidunt ut labore et dolore magna.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="row justify-content-center">
+            <div className="col-lg-4 col-md-6 col-sm-8 col-11">
+              <div className="single-service">
+                <div className="icon">
+                  <i className="lni lni-book" />
+                </div>
+                <div className="service-content">
+                  <h3>FULLY DOCUMENTED</h3>
+                  <p>
+                    Buy and sell everything from used cars to mobile phones and
+                    computer or search for property.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4 col-md-6 col-sm-8 col-11">
+              <div className="single-service">
+                <div className="icon">
+                  <i className="lni lni-leaf" />
+                </div>
+                <div className="service-content">
+                  <h3>CLEAN &amp; MODERN DESIGN</h3>
+                  <p>
+                    Buy and sell everything from used cars to mobile phones and
+                    computer or search for property.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4 col-md-6 col-sm-8 col-11">
+              <div className="single-service">
+                <div className="icon">
+                  <i className="lni lni-map" />
+                </div>
+                <div className="service-content">
+                  <h3>GREAT FEATURES</h3>
+                  <p>
+                    Buy and sell everything from used cars to mobile phones and
+                    computer or search for property.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4 col-md-6 col-sm-8 col-11">
+              <div className="single-service">
+                <div className="icon">
+                  <i className="lni lni-cog" />
+                </div>
+                <div className="service-content">
+                  <h3>COMPLETELY CUSTOMIZABLE</h3>
+                  <p>
+                    Buy and sell everything from used cars to mobile phones and
+                    computer or search for property.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4 col-md-6 col-sm-8 col-11">
+              <div className="single-service">
+                <div className="icon">
+                  <i className="lni lni-pointer-up" />
+                </div>
+                <div className="service-content">
+                  <h3>USER FRIENDLY</h3>
+                  <p>
+                    Buy and sell everything from used cars to mobile phones and
+                    computer or search for property.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4 col-md-6 col-sm-8 col-11">
+              <div className="single-service">
+                <div className="icon">
+                  <i className="lni lni-laptop-phone" />
+                </div>
+                <div className="service-content">
+                  <h3>FULLY RESPONSIVE</h3>
+                  <p>
+                    Buy and sell everything from used cars to mobile phones and
+                    computer or search for property.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
